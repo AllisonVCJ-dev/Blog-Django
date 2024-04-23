342422085b91612e5f7258603735f75f8be392bc
AllisonVCJ-dev <allisonvaldocorrea14@gmail.com>
2024-04-23 16:11:16 -0300

    django-summernote, finalização dos posts, safe, CodeMirror, Tags, Search, Http404, ClassBasedView

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,7 @@
-from blog.models import Category, Tag, Page, Post
+from blog.models import Category, Page, Post, Tag
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from django.utils.safestring import mark_safe
 
 
 @admin.register(Tag)
@@ -12,8 +14,6 @@ class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('name',),
     }
-
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
@@ -25,8 +25,10 @@ class CategoryAdmin(admin.ModelAdmin):
         "slug": ('name',),
     }
 
+
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'content',
@@ -38,8 +40,10 @@ class PageAdmin(admin.ModelAdmin):
         "slug": ('title',),
     }
 
+
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published',  'created_by',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
@@ -47,15 +51,29 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = 'category', 'is_published',
     list_editable = 'is_published',
     ordering = '-id',
-    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    readonly_fields = (
+        'created_at', 'updated_at', 'created_by', 'updated_by',
+        'link',
+    )
     prepopulated_fields = {
         "slug": ('title',),
     }
     autocomplete_fields = 'tags', 'category',
 
+    def link(self, obj):
+        if not obj.pk:
+            return '-'
+
+        url_do_post = obj.get_absolute_url()
+        safe_link = mark_safe(
+            f'<a target="_blank" href="{url_do_post}">Ver post</a>'
+        )
+
+        return safe_link
+    
     def save_model(self, request, obj, form, change):
         if change:
-            obj.updated_by = request.user  
+            obj.updated_by = request.user 
         else:
-            obj.created_by = request.user  
+            obj.created_by = request.user 
         obj.save()
